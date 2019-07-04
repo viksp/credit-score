b1826f2c354bcf3a97d3e2d11d3f8208a96a9dbf
Vikas <viksp03@gmail.com>
2019-07-04 22:48:22 +0530

    Removed Header.js file as it was not getting used

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/Header'
 import Sidebar from './components/sidebar/Sidebar'
 import Main from './components/main/Main';
 import {BrowserRouter as Router} from 'react-router-dom';
